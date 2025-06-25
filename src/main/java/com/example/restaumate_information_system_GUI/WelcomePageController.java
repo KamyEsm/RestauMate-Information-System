78b83b4c4e01adf5097276690bf17a7b4d8e7ab2
kamy <130361402+KamyEsm@users.noreply.github.com>
2025-06-25 20:29:29 +0330

    Creat WelComePage

---
+++ src/main/java/com/example/restaumate_information_system_GUI/WelcomePageController.java
@@ -0,0 +1,18 @@
+package com.example.restaumate_information_system_GUI;
+
+import javafx.event.ActionEvent;
+
+public class WelcomePageController {
+    public void AdminManager(ActionEvent actionEvent) {
+
+    }
+
+    public void FoodManager(ActionEvent actionEvent) {
+    }
+
+    public void MemberManager(ActionEvent actionEvent) {
+    }
+
+    public void TableManager(ActionEvent actionEvent) {
+    }
+}
