8c2dd5c9fbb47961e5e643b64a5b27b0bb6a07ad
kamy <130361402+KamyEsm@users.noreply.github.com>
2025-06-25 15:04:25 +0330

    Add IManager Interface

---
+++ src/main/java/Interface/IManager.java
@@ -0,0 +1,12 @@
+package Interface;
+
+public interface IManager {
+    public void Append(String... Rows);
+    public void Delete(String Row);
+    public void Delete(int Row);
+    public String Search(String Row);
+    public String Search(int Row, String Row1);
+    public void Update(String Row,String Row1);
+    public void Update(int Row,String Row1);
+    public void Insert(int S,String Row1);
+}
