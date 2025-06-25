948475c05d7cd813f1cb29e9a7c86e59ae18fcf9
kamy <130361402+KamyEsm@users.noreply.github.com>
2025-06-25 17:05:11 +0330

    Update Food Entity

--- src/main/java/Commons/Food.java
+++ src/main/java/Commons/Food.java
@@ -4,18 +4,18 @@ import Splitter.Splitter;
 
 public class Food {
     private String name;
-    private String category;
+    private CategoryFood category;
     private double price;
     public Food(String name, String category, double price) {}
     public Food() {}
 
     public String getName() { return name; }
-    public String getCategory() { return category; }
+    public CategoryFood getCategory() { return category; }
     public double getPrice() { return price; }
     public void setName(String name) { this.name = name; }
-    public void setCategory(String category) { this.category = category; }
+    public void setCategory(CategoryFood category) { this.category = category; }
     public void setPrice(double price) { this.price = price; }
     public String toString() {
-        return name + Splitter.SEPARATOR + category + Splitter.SEPARATOR + price;
+        return name + Splitter.SEPARATOR + category.toString() + Splitter.SEPARATOR + price;
     }
 }
