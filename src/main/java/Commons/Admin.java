ddc13d329355d0d43902932745d94394875619a1
kamy <130361402+KamyEsm@users.noreply.github.com>
2025-06-25 12:14:27 +0330

    Creat Admin Entity

---
+++ src/main/java/Commons/Admin.java
@@ -0,0 +1,29 @@
+package Commons;
+
+public class Admin extends Member {
+    private boolean Owner;
+    private String Password;
+    public Admin() {}
+    public Admin(int ID, String Name, String LastName, int Age, String NationalCode, Gender gender
+    , boolean Owner, String Password) {
+        super(ID, Name, LastName, Age, NationalCode, gender);
+        SetOwner(Owner);
+        SetPassword(Password);
+    }
+
+    public boolean isOwner() {
+        return Owner;
+    }
+    public void SetOwner(boolean Owner) {
+        this.Owner = Owner;
+    }
+    public String GetPassword() {
+        return Password;
+    }
+    public boolean SetPassword(String Password) {
+        if(Password.indexOf(" ")!=-1)
+            return false;
+        this.Password = Password;
+        return true;
+    }
+}
