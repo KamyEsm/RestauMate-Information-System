8181c37315008ddb66946b36f8bdb17380ad8d77
kamy <130361402+KamyEsm@users.noreply.github.com>
2025-06-26 14:51:38 +0330

    Develop WelcomePageController.java

--- src/main/java/com/example/restaumate_information_system_GUI/WelcomePageController.java
+++ src/main/java/com/example/restaumate_information_system_GUI/WelcomePageController.java
@@ -1,18 +1,62 @@
 package com.example.restaumate_information_system_GUI;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class WelcomePageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WelcomePageController implements Initializable {
+    private Stage stage;
     public void AdminManager(ActionEvent actionEvent) {
-
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/restaumate_information_system/AdminPage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+        }
+        catch (Exception e){
+            System.err.println(e.getMessage());
+        }
     }
 
     public void FoodManager(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/restaumate_information_system/FoodPage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+        }
+        catch (Exception e){
+            System.err.println(e.getMessage());
+        }
     }
 
     public void MemberManager(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/restaumate_information_system/MemberPage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+        }
+        catch (Exception e){
+            System.err.println(e.getMessage());
+        }
     }
 
     public void TableManager(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/restaumate_information_system/TablePage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+        }
+        catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        stage = StartApplication.getPrimaryStage();
     }
 }
